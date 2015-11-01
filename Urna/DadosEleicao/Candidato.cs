bccaae25d85f4b0e214ad937feefe7e6f0c6e40e
Daniele <daniele.m.paz@outlook.com>
2015-11-01 15:37:04 -0200

    feat(Candidato): adicionando classe Candidato

--- Urna/DadosEleicao/Candidato.cs
+++ Urna/DadosEleicao/Candidato.cs
@@ -8,5 +8,26 @@ namespace DadosEleicao
 {
    class Candidato
     {
+        public int IDCandidato { get; private set; }
+        public string NomeCompleto { get; private set; }
+        public string NomePopular { get; private set; }
+        public int DataNascimento { get; private set; }
+        public int RegistroTRE { get; private set; }
+        public int IDPartido { get; private set; }
+        public int Numero { get; private set; }
+        public int IDCargo { get; private set; }
+        public bool Exibe { get; private set; }
+
+        public Candidato(String NomeCompleto, String NomePopular, int DataNascimento, int RegistroTRE, int IDPartido, int Numero, int IDCargo)
+        {
+            this.NomeCompleto = NomeCompleto;
+            this.NomePopular = NomePopular;
+            this.DataNascimento = DataNascimento;
+            this.RegistroTRE = RegistroTRE;
+            this.IDPartido = IDPartido;
+            this.Numero = Numero;
+            this.IDCargo = IDCargo;
+
+        }
     }
 }
